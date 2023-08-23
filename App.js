d8eae615bd64ec13885444044badccb3ec745726
lipestile <felipebalbino53@gmail.com>
2023-08-23 19:38:54 -0300

    teste-em-componente

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import Minmax from './Components/Minmax';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Minmax minimo='1000' maximo='599' />
+      <Image>
+
+      </Image>
     </View>
   );
 }
---
+++ Components/Minmax.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+export default function Minmax(props)
+{
+    const {maximo, minimo} = props;
+     
+    const maxvalue = Math.max(parseInt(minimo), parseInt(maximo));
+    
+
+    return (
+        <Text>
+            O valor maximo é:{maxvalue}
+        </Text>
+    )
+}
+
+
